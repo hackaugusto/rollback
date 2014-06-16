20bbf325c8f1f50b5622737eff43a298ac784651
Augusto F. Hack <hack.augusto@gmail.com>
2014-06-15 23:33:46 -0300

    fix setup.py test

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ from setuptools import setup
 from setuptools.command.test import test
 
 class Tox(test):
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
     def run_tests(self):
         import tox
         sys.exit(tox.cmdline())
